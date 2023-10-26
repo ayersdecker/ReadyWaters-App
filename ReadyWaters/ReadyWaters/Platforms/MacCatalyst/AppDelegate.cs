8c2600d031a3c46ff4a2baf6f22500f5342e816e
Decker Ayers <ayersdecker@gmail.com>
2023-10-26 11:55:28 -0400

    Initial Frame

---
+++ ReadyWaters/ReadyWaters/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ReadyWaters
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
