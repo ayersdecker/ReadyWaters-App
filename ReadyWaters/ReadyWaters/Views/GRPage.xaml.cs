00dc70fddabf08d011feb604e1fc8db274cebbf5
Decker Ayers <ayersdecker@gmail.com>
2023-11-02 11:13:12 -0400

    Added Location Data

--- ReadyWaters/ReadyWaters/Views/GRPage.xaml.cs
+++ ReadyWaters/ReadyWaters/Views/GRPage.xaml.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace ReadyWaters.Views;
 
 public partial class GRPage : ContentPage
@@ -5,5 +7,30 @@ public partial class GRPage : ContentPage
 	public GRPage()
 	{
 		InitializeComponent();
-	}
+        OnGetForecast(43.2540828, -77.6017813);
+    }
+    public async void OnGetForecast(double lon, double lat)
+    {
+        dynamic query = await FetchURLToJson($"https://api.weather.gov/points/{lon},{lat}");
+        string forecastUrl = query.properties.forecast;
+        dynamic forecast = await FetchURLToJson(forecastUrl);
+
+        string forecastText = forecast.properties.periods[0].detailedForecast;
+        AirTempField.Text = forecast.properties.periods[0].temperature + " F";
+        string humidity = forecast.properties.periods[0].relativeHumidity + "%";
+        WindVelocityField.Text = forecast.properties.periods[0].windSpeed;
+        WindDirectionField.Text = forecast.properties.periods[0].windDirection;
+
+    }
+    public async Task<dynamic> FetchURLToJson(string url)
+    {
+        HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
+        client.DefaultRequestHeaders.Add("User-Agent", "WaterReady");
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+        HttpResponseMessage response = await client.GetAsync(url);
+        //response.EnsureSuccessStatusCode();
+        string content = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<dynamic>(content);
+    }
 }
--- ReadyWaters/ReadyWaters/Views/LocationPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace ReadyWaters.Views;
-
-public partial class LocationPage : ContentPage
-{
-	public LocationPage()
-	{
-		InitializeComponent();
-	}
-}
