fd555e51504a5f072c97d24be546d8f4d311ba5a
Decker Ayers <ayersdecker@gmail.com>
2023-11-13 11:50:23 -0500

    Charts are in, need to get data loading from IBViewModel ASAP

---
+++ ReadyWaters/ReadyWaters/ViewModels/BaseViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ReadyWaters.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+        public event PropertyChangedEventHandler? PropertyChanged;
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+
+        public ObservableCollection<Brush> PaletteBrushes { get; set; }
+        public ObservableCollection<Brush> CustomColor2 { get; set; }
+        public ObservableCollection<Brush> CustomColor3 { get; set; }
+
+        public ObservableCollection<Brush> CustomColor4 { get; set; }
+        public ObservableCollection<Brush> AlterColor1 { get; set; }
+
+        private bool enableAnimation = true;
+        public bool EnableAnimation
+        {
+            get { return enableAnimation; }
+            set
+            {
+                enableAnimation = value;
+                OnPropertyChanged("EnableAnimation");
+            }
+        }
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public BaseViewModel()
+        {
+            PaletteBrushes = new ObservableCollection<Brush>()
+            {
+               new SolidColorBrush(Color.FromArgb("#FFFFFF")),
+                 new SolidColorBrush(Color.FromArgb("#FFFFFF")),
+                 new SolidColorBrush(Color.FromArgb("#FFFFFF")),
+                 new SolidColorBrush(Color.FromArgb("#FFFFFF")),
+                 new SolidColorBrush(Color.FromArgb("#FFFFFF"))
+            };
+            CustomColor2 = new ObservableCollection<Brush>()
+            {
+                new SolidColorBrush(Color.FromArgb("#ec41f2"))
+            };
+
+            CustomColor3 = new ObservableCollection<Brush>()
+            {
+                new SolidColorBrush(Color.FromArgb("#1c43ed"))
+            };
+
+            CustomColor4 = new ObservableCollection<Brush>()
+            {
+                new SolidColorBrush(Color.FromArgb("#95DB9C")),
+                new SolidColorBrush(Color.FromArgb("#B95375")),
+                new SolidColorBrush(Color.FromArgb("#56BBAF")),
+                new SolidColorBrush(Color.FromArgb("#606D7F")),
+                new SolidColorBrush(Color.FromArgb("#E99941")),
+                new SolidColorBrush(Color.FromArgb("#327DBE")),
+                new SolidColorBrush(Color.FromArgb("#E7695A")),
+                new SolidColorBrush(Color.FromArgb("#2D4552")),
+                new SolidColorBrush(Color.FromArgb("#4E9B8F")),
+                new SolidColorBrush(Color.FromArgb("#E9A56C")),
+            };
+
+
+            AlterColor1 = new ObservableCollection<Brush>()
+            {
+                new SolidColorBrush(Color.FromArgb("#314A6E")),
+                 new SolidColorBrush(Color.FromArgb("#48988B")),
+            };
+        }
+    }
+}
---
+++ ReadyWaters/ReadyWaters/ViewModels/IBViewModel.cs
@@ -0,0 +1,95 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using SkiaSharp;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+
+namespace ReadyWaters.ViewModels
+{
+    public class IBViewModel : BaseViewModel
+    {
+        public ObservableCollection<ChartDataModel> LineData1 { get; set; }
+
+        public ObservableCollection<ChartDataModel> LineData2 { get; set; }
+
+        public ObservableCollection<ChartDataModel> LineData3 { get; set; }
+
+        public IBViewModel()
+        {
+
+            LineData1 = new ObservableCollection<ChartDataModel>
+            {
+                new ChartDataModel("Now", 56),
+                new ChartDataModel("+1", 44),
+                new ChartDataModel("+2", 48),
+                new ChartDataModel("+3", 50),
+                new ChartDataModel("+4", 66),
+                new ChartDataModel("+5", 78)
+            };
+
+            LineData2 = new ObservableCollection<ChartDataModel>
+            {
+                new ChartDataModel("Now", 56),
+                new ChartDataModel("+1", 44),
+                new ChartDataModel("+2", 48),
+                new ChartDataModel("+3", 50),
+                new ChartDataModel("+4", 66),
+                new ChartDataModel("+5", 78)
+            };
+
+            LineData3 = new ObservableCollection<ChartDataModel>
+            {
+                new ChartDataModel("Now", 0),
+                new ChartDataModel("+1", 0),
+                new ChartDataModel("+2", 0),
+                new ChartDataModel("+3", 0),
+                new ChartDataModel("+4", 0),
+                new ChartDataModel("+5", 0)
+            };
+        }
+        public async Task<dynamic> OnGetForecast(double lon, double lat)
+        {
+            dynamic query = await FetchURLToJson($"https://api.weather.gov/points/{lon},{lat}");
+            return query.properties.forecastHourly;
+
+            //string forecastText = forecast.properties.periods[0].detailedForecast;
+            //AirTempField.Text = forecast.properties.periods[0].temperature + " F";
+            //string humidity = forecast.properties.periods[0].relativeHumidity + "%";
+            //WindVelocityField.Text = forecast.properties.periods[0].windSpeed;
+            //WindDirectionField.Text = forecast.properties.periods[0].windDirection;
+
+        }
+        public async Task<dynamic> FetchURLToJson(string url)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
+            client.DefaultRequestHeaders.Add("User-Agent", "WaterReady");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            HttpResponseMessage response = await client.GetAsync(url);
+            //response.EnsureSuccessStatusCode();
+            string content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<dynamic>(content);
+        }
+        public ObservableCollection<ChartDataModel> GetAirTemp(dynamic dynamic)
+        {
+            ObservableCollection<double> airTemps = new ObservableCollection<double>();
+            foreach (var period in dynamic.properties.periods.Take(5))
+            {
+                double temperature = period.temperature;
+                airTemps.Add(temperature);
+            }
+            return new ObservableCollection<ChartDataModel>
+            {
+                new ChartDataModel("Now", airTemps[0]),
+                new ChartDataModel("+1", airTemps[1]),
+                new ChartDataModel("+2", airTemps[2]),
+                new ChartDataModel("+3", airTemps[3]),
+                new ChartDataModel("+4", airTemps[4]),
+                new ChartDataModel("+5", airTemps[5])
+            };
+        }
+    }
+}
