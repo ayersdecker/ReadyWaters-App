c0865d9fdd7c8e9bf06a83ceb5f0341fd8a5e974
Decker Ayers <ayersdecker@gmail.com>
2023-11-26 16:23:13 -0500

    Working Slider WITHOUT Animation, Major Thread Issues

--- ReadyWaters/ReadyWaters/Views/LVPage.xaml.cs
+++ ReadyWaters/ReadyWaters/Views/LVPage.xaml.cs
@@ -1,16 +1,28 @@
+using Microsoft.Maui.Platform;
+
 namespace ReadyWaters.Views;
 
 public partial class LVPage : ContentPage
 {
-    int count = 1;
-	public LVPage()
+
+
+    public LVPage()
 	{
-		InitializeComponent();
-	}
-    private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        InitializeComponent();
+    }
+
+   
+
+
+
+    private void Slider_ValueChanged(object sender, Syncfusion.Maui.Sliders.SliderValueChangedEventArgs e)
     {
-        double value = e.NewValue;
-        string url = $"https://www.glerl.noaa.gov/res/glcfs/ont/frames//uv_{value}.png?1699262749509";
-        Chart1.Source = url;
+        double num = Slider.Value;
+        if (num > 0) { Chart1Label.Text = $"UV Index for {num * 3} hours from now"; Chart2Label.Text = $"Water Temperature for {num * 3 hours from now";}
+        if (num == 0) { Chart1Label.Text = $"UV Index for now"; Chart2Label.Text = $"Water Temperature for now"; }
+        HapticFeedback.Default.Perform(HapticFeedbackType.Click);
+        Chart1.Source = $"https://www.glerl.noaa.gov/res/glcfs/ont/frames//uv_{num + 49}.png?1699262749509";
+        Chart2.Source = $"https://www.glerl.noaa.gov/res/glcfs/ont/frames//temp_{num + 49}.png";
+          
     }
 }
