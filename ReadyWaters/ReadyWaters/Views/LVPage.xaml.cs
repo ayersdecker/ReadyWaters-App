29af626a694777997923e4ec37a6399fe1179ab8
Decker Ayers <ayersdecker@gmail.com>
2023-11-26 23:53:45 -0500

    Fix

--- ReadyWaters/ReadyWaters/Views/LVPage.xaml.cs
+++ ReadyWaters/ReadyWaters/Views/LVPage.xaml.cs
@@ -18,7 +18,7 @@ public partial class LVPage : ContentPage
     private void Slider_ValueChanged(object sender, Syncfusion.Maui.Sliders.SliderValueChangedEventArgs e)
     {
         double num = Slider.Value;
-        if (num > 0) { Chart1Label.Text = $"UV Index for {num * 3} hours from now"; Chart2Label.Text = $"Water Temperature for {num * 3 hours from now";}
+        if (num > 0) { Chart1Label.Text = $"UV Index for {num * 3} hours from now"; Chart2Label.Text = $"Water Temperature for {num * 3} hours from now";}
         if (num == 0) { Chart1Label.Text = $"UV Index for now"; Chart2Label.Text = $"Water Temperature for now"; }
         HapticFeedback.Default.Perform(HapticFeedbackType.Click);
         Chart1.Source = $"https://www.glerl.noaa.gov/res/glcfs/ont/frames//uv_{num + 49}.png?1699262749509";
