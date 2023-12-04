43002c9d9ed78017a6f43846d508f3aa2fabf51f
Decker Ayers <ayersdecker@gmail.com>
2023-12-04 00:16:28 -0500

    Labels Fixed

--- ReadyWaters/ReadyWaters/Views/LVPage.xaml.cs
+++ ReadyWaters/ReadyWaters/Views/LVPage.xaml.cs
@@ -16,8 +16,6 @@ public partial class LVPage : ContentPage
     // Selection Options
     public List<string> ChartOptions = new List<string> { "Surface Currents", "Surface Temperatures", "Water Levels" };
 
-    public string actualForecastHour = "";
-
     public LVPage()
     {
         InitializeComponent();
@@ -50,8 +48,7 @@ public partial class LVPage : ContentPage
         Chart2.Source = String.Format(SelectedChart2, (currentHour/3) + 49);
 
         // Set the default chart labels
-        Chart1Label.Text = $"{Chart1Select.SelectedItem} ({actualForecastHour} forecast)";
-        Chart2Label.Text = $"{Chart2Select.SelectedItem} ({actualForecastHour} forecast)";
+        ChartLabelRefresh();
     }
 
     // Method to handle the chart selection event
@@ -82,94 +79,32 @@ public partial class LVPage : ContentPage
                 Chart2.Source = string.Format(SelectedChart2, 49 + Slider.Value);
             }
         }
+        ChartLabelRefresh();
     }
 
     private void ChartLabelRefresh()
     {
-        DateTime initialTime = DateTime.Now;
-        string initialTimeToString = (initialTime).ToString("h:mm tt");
-        string amPm = initialTime.ToString("tt", System.Globalization.CultureInfo.InvariantCulture);
-        //string hour = initialTime.ToString("h", System.Globalization.CultureInfo.InvariantCulture);
-        DateTime initialtimeParsed = DateTime.Parse(initialTimeToString);
-        int hourParsed = initialtimeParsed.Hour;
-        int initialHour = initialTime.Hour;
-        
-
-
-        /*I was trying to make the label for the chart update/respond properly according to the selection on the slider so that
-         it would say what forecast it is displaying; meaning the midnight forecast, the 3 o'clock forecast
-        6, or 9. Currently it is just displaying the current time as if it is the current forecast of that hour. This needs
-        to be fixed.*/
-        actualForecastHour="";
-
-        //if ((initialHour >= 0 && initialHour < 3) || initialHour == 24) {actualForecastHour = "12"; }
-        //if (initialHour >= 3 && initialHour < 6 || initialHour >= 15 && initialHour < 18) {actualForecastHour = "3"; }
-        //if (initialHour >= 6 && initialHour < 9 || initialHour >= 18 && initialHour < 21) {actualForecastHour = "6"; }
-        //if (initialHour >= 9 && initialHour < 12 || initialHour >= 21 && initialHour < 24) {actualForecastHour = "9"; }
-        
-
+        DateTime currentTime = DateTime.Now;
         double num = Slider.Value;
-
-        if (num == 0)
+        int hourGroup = (currentTime.Hour / 3) * 3;
+        string ampm = currentTime.Hour < 12 ? "am" : "pm";
+        if (hourGroup + num >= 24)
         {
-            
-            //I tried in this first chart label to have it respond correctly
-            Chart1Label.Text = $"{Chart1Select.SelectedItem} ({actualForecastHour} {amPm}) forecast";
+            hourGroup -= 24;
+            ampm = "am";
+        }
 
-           //I initially just used the current time to display on this second chart label
-           Chart2Label.Text = $"{Chart2Select.SelectedItem}: ({actualForecastHour} {amPm}) forecast)";        
-
+            if(num == 0)
+            {
+                Chart1Label.Text = $"  {Chart1Select.SelectedItem}: {hourGroup} {ampm}";
+                Chart2Label.Text = $"  {Chart2Select.SelectedItem}: {hourGroup} {ampm}";
+            }
+            else
+            {
+                Chart1Label.Text = $"  {Chart1Select.SelectedItem}: {hourGroup + num} {ampm}";
+                Chart2Label.Text = $"  {Chart2Select.SelectedItem}: {hourGroup + num} {ampm}";
+            }
             return;
-        }
-        else if(num == 3) 
-        {
-            initialHour = initialTime.Hour + 3;
-            if ((initialHour >= 0 && initialHour < 3) || initialHour > 21) { actualForecastHour = "12"; }
-            if (initialHour >= 3 && initialHour < 6 || initialHour >= 12 && initialHour < 15) { actualForecastHour = "3"; }
-            if (initialHour >= 6 && initialHour < 9 || initialHour >= 15 && initialHour < 18) { actualForecastHour = "6"; }
-            if (initialHour >= 9 && initialHour < 12 || initialHour >= 18 && initialHour < 21) { actualForecastHour = "9"; }
-
-            //int actualForecastHourConverted = int.Parse(actualForecastHour);
-            Chart1Label.Text = $"{Chart1Select.SelectedItem}: ({actualForecastHour} {amPm} forecast)";
-            Chart2Label.Text = $"{Chart2Select.SelectedItem}: {num * 3} hours from now";
-            return;
-        }
-        else if (num == 6)
-        {
-            initialHour = initialHour + 6;
-            if ((initialHour >= 0 && initialHour < 3) || initialHour > 21 && initialHour < 27) { actualForecastHour = "12"; }
-            if (initialHour >= 3 && initialHour < 6 || initialHour >= 12 && initialHour < 15 || initialHour >= 27 && initialHour <30) { actualForecastHour = "3"; }
-            if (initialHour >= 6 && initialHour < 9 || initialHour >= 15 && initialHour < 18 || initialHour >= 30 && initialHour <33) { actualForecastHour = "6"; }
-            if (initialHour >= 9 && initialHour < 12 || initialHour >= 18 && initialHour < 21) { actualForecastHour = "9"; }
-
-            //int actualForecastHourConverted = int.Parse(actualForecastHour);
-            Chart1Label.Text = $"{Chart1Select.SelectedItem}: ({actualForecastHour} {amPm} forecast)";
-            Chart2Label.Text = $"{Chart2Select.SelectedItem}: {num * 3} hours from now";
-        }
-        else if (num == 9)
-        {
-            initialHour = initialHour + 9;
-            if ((initialHour >= 0 && initialHour < 3) || initialHour > 21) { actualForecastHour = "12"; }
-            if (initialHour >= 3 && initialHour < 6 || initialHour >= 12 && initialHour < 15) { actualForecastHour = "3"; }
-            if (initialHour >= 6 && initialHour < 9 || initialHour >= 15 && initialHour < 18) { actualForecastHour = "6"; }
-            if (initialHour >= 9 && initialHour < 12 || initialHour >= 18 && initialHour < 21) { actualForecastHour = "9"; }
-
-            //int actualForecastHourConverted = int.Parse(actualForecastHour);
-            Chart1Label.Text = $"{Chart1Select.SelectedItem}: ({actualForecastHour} {amPm} forecast)";
-            Chart2Label.Text = $"{Chart2Select.SelectedItem}: ({actualForecastHour} {amPm} forecast)";
-        }
-        else if (num == 12)
-        {
-            initialHour = initialHour + 12;
-            if ((initialHour >= 0 && initialHour < 3) || initialHour > 21) { actualForecastHour = "12"; }
-            if (initialHour >= 3 && initialHour < 6 || initialHour >= 12 && initialHour < 15) { actualForecastHour = "3"; }
-            if (initialHour >= 6 && initialHour < 9 || initialHour >= 15 && initialHour < 18) { actualForecastHour = "6"; }
-            if (initialHour >= 9 && initialHour < 12 || initialHour >= 18 && initialHour < 21) { actualForecastHour = "9"; }
-
-            //int actualForecastHourConverted = int.Parse(actualForecastHour);
-            Chart1Label.Text = $"{Chart1Select.SelectedItem}: ({actualForecastHour} {amPm} forecast)";
-            Chart2Label.Text = $"{Chart2Select.SelectedItem}: ({actualForecastHour} {amPm} forecast)";
-        }
     }
 
     // Method to handle the slider value change event
